dcd6fb4ea3ba9fbc64ef8dc1fc614ba043ee3479
Oktober Comstock <oktober.comstock@gmail.com>
2015-12-21 14:57:08 -0800

    implemented index action

--- app/controllers/pins_controller.rb
+++ app/controllers/pins_controller.rb
@@ -1,9 +1,16 @@
 class PinsController < ApplicationController
+  before_action :find_pin, only: [:show, :edit, :update, :destroy]
+
   def index
+    @pins = Pin.all.order("created_at DESC")
   end
 
   def new
     @pin = Pin.new
+  end
+
+  def show
+
   end
 
   def create
@@ -20,5 +27,9 @@ class PinsController < ApplicationController
 
   def pin_params
     params.require(:pin).permit(:title, :description)
+  end
+
+  def find_pin
+    @pin = Pin.find(params[:id])
   end
 end
